e3044a30531b390d32f66393191bdff8eb7522de
Brendan Ward <kingbeebop@gmail.com>
2022-06-27 16:38:46 -0400

    commit tree 197c8880fcfc1dd78eddacaafab7f23541e01594

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
 
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+        @page_count = 0
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
